5eb8916cc1910c2110a307f9a79773b3ee737d87
Diego Almeida <diegoalmda@gmail.com>
2022-03-26 18:43:37 -0300

    Calendar component customized

---
+++ src/screens/Scheduling/index.tsx
@@ -0,0 +1,79 @@
+import React from 'react';
+import { useTheme } from 'styled-components';
+
+import { BackButton } from '../../components/BackButton';
+
+import ArrowSvg from '../../assets/arrow.svg';
+
+import {
+  Container,
+  Header,
+  Title,
+  RentalPeriod,
+  DateInfo,
+  DateTitle,
+  DateValue,
+  DateValueContainer,
+  Content,
+  Footer
+} from './styles';
+import { StatusBar } from 'react-native';
+import { Button } from '../../components/Button';
+import { Calendar } from '../../components/Calendar';
+
+export function Scheduling() {
+    const theme = useTheme();
+
+  return (
+    <Container>
+        <Header>
+            <StatusBar 
+                barStyle="light-content"
+                translucent
+                backgroundColor="transparent"
+            />
+            <BackButton 
+                onPress={() => {}} 
+                color={theme.colors.shape}
+            />
+
+            <Title>
+                Escolha uma {'\n'}
+                data de início e {'\n'}
+                fim do aluguel
+            </Title>
+
+            <RentalPeriod>
+                <DateInfo>
+                    <DateTitle>DE</DateTitle>
+                    <DateValueContainer selected={false}>
+                        <DateValue selected={false}>
+                            26/03/2022
+                        </DateValue>
+                    </DateValueContainer>
+                </DateInfo>
+
+                <ArrowSvg />
+
+                <DateInfo>
+                    <DateTitle>ATÉ</DateTitle>
+                    <DateValueContainer selected={false}>
+                        <DateValue selected={false}>
+                            26/03/2022
+                        </DateValue>
+                    </DateValueContainer>
+                </DateInfo>
+            </RentalPeriod>
+
+        </Header>
+
+        <Content>
+            <Calendar />
+        </Content>
+
+        <Footer>
+            <Button title="Confirmar" />
+        </Footer>
+    </Container>
+  );
+}
