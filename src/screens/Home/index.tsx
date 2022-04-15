a629e37186f7bcc3c97161310c7ba42d37e32a8c
Diego Almeida <diegoalmda@gmail.com>
2022-04-15 15:59:04 -0300

    App loading and go back adjustments

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { StatusBar, StyleSheet } from 'react-native';
+import { StatusBar, StyleSheet, BackHandler } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 import { RFValue } from 'react-native-responsive-fontsize';
 import { Ionicons } from '@expo/vector-icons';
@@ -88,6 +88,12 @@ export function Home() {
     fetchCars();
   }, []);
 
+  useEffect(() => {
+    BackHandler.addEventListener('hardwareBackPress', () => {
+      return true;
+    })
+  }, []);
+
   return (
     <Container>
       <StatusBar 
@@ -101,9 +107,12 @@ export function Home() {
                   width={RFValue(108)}
                   height={RFValue(12)}
               />   
-              <TotalCars>
-                  Total de {cars.length} carros    
-              </TotalCars>      
+              {
+                !loading &&
+                <TotalCars>
+                    Total de {cars.length} carros    
+                </TotalCars>     
+              } 
           </HeaderContent>
       </Header>
       { loading ? <Load /> :
