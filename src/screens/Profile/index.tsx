776e8f3bd7ac90dacb9379abd1ffdda59999e57a
Diego Almeida <diegoalmda@gmail.com>
2022-04-25 18:18:45 -0300

    Created profile screen

--- src/screens/Profile/index.tsx
+++ src/screens/Profile/index.tsx
@@ -1,9 +1,16 @@
 import React, { useState } from 'react';
+import {
+  Keyboard,
+  KeyboardAvoidingView,
+  TouchableWithoutFeedback
+} from 'react-native';
 import { useNavigation } from '@react-navigation/native';
+import { useBottomTabBarHeight } from '@react-navigation/bottom-tabs';
 import { useTheme } from 'styled-components';
 import { Feather } from '@expo/vector-icons';
 
 import { BackButton } from '../../components/BackButton';
+import { useAuth } from '../../hooks/auth';
 
 import {
   Container,
@@ -20,9 +27,13 @@ import {
   OptionTitle,
   Section
 } from './styles';
+import { Input } from '../../components/Input';
+import { PasswordInput } from '../../components/PasswordInput';
 
 export function Profile() {
   const [option, setOption] = useState<'dataEdit' | 'passwordEdit'>('dataEdit');
+
+  const { user } = useAuth();
 
   const theme = useTheme();
   const navigation = useNavigation();
@@ -40,59 +51,100 @@ export function Profile() {
   }
 
   return (
-    <Container>
-      <Header>
-        <HeaderTop>
-          <BackButton 
-            color={theme.colors.shape} 
-            onPress={handleBack} 
-          />
-          <HeaderTitle>Editar Perfil</HeaderTitle>
-          <LogoutButton onPress={handleSignOut} >
-            <Feather 
-              name="power" 
-              size={24} 
-              color={theme.colors.shape} 
-            />
-          </LogoutButton>
-        </HeaderTop>
+    <KeyboardAvoidingView behavior="position" enabled>
+      <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+        <Container>
+          <Header>
+            <HeaderTop>
+              <BackButton 
+                color={theme.colors.shape} 
+                onPress={handleBack} 
+              />
+              <HeaderTitle>Editar Perfil</HeaderTitle>
+              <LogoutButton onPress={handleSignOut} >
+                <Feather 
+                  name="power" 
+                  size={24} 
+                  color={theme.colors.shape} 
+                />
+              </LogoutButton>
+            </HeaderTop>
 
-        <PhotoContainer>
-          <Photo source={{ uri: 'https://avatars.githubusercontent.com/u/70728380?v=4' }} />
-          <PhotoButton onPress={() => {}} >
-            <Feather 
-              name="camera" 
-              size={24} 
-              color={theme.colors.shape} 
-            />
-          </PhotoButton>
-        </PhotoContainer>
-      </Header>
+            <PhotoContainer>
+              <Photo source={{ uri: 'https://avatars.githubusercontent.com/u/70728380?v=4' }} />
+              <PhotoButton onPress={() => {}} >
+                <Feather 
+                  name="camera" 
+                  size={24} 
+                  color={theme.colors.shape} 
+                />
+              </PhotoButton>
+            </PhotoContainer>
+          </Header>
 
-      <Content>
-        <Options>
-          <Option 
-            active={option === 'dataEdit'}
-            onPress={() => handleOptionChange('dataEdit')}
-          >
-            <OptionTitle active={option === 'dataEdit'}>
-              Dados
-            </OptionTitle>
-          </Option>
-          <Option 
-            active={option === 'passwordEdit'}
-            onPress={() => handleOptionChange('passwordEdit')}
-          >
-            <OptionTitle active={option === 'passwordEdit'}>
-              Trocar senha
-            </OptionTitle>
-          </Option>
-        </Options>
+          <Content style={{ marginBottom: useBottomTabBarHeight() }}>
+            <Options>
+              <Option 
+                active={option === 'dataEdit'}
+                onPress={() => handleOptionChange('dataEdit')}
+              >
+                <OptionTitle active={option === 'dataEdit'}>
+                  Dados
+                </OptionTitle>
+              </Option>
+              <Option 
+                active={option === 'passwordEdit'}
+                onPress={() => handleOptionChange('passwordEdit')}
+              >
+                <OptionTitle active={option === 'passwordEdit'}>
+                  Trocar senha
+                </OptionTitle>
+              </Option>
+            </Options>
 
-        <Section>
+            {
+              option === 'dataEdit' ?
 
-        </Section>
-      </Content>
-    </Container>
+              <Section>
+                <Input 
+                  iconName="user"
+                  placeholder='Nome'
+                  autoCorrect={false}
+                  defaultValue={user.name}
+                />
+                <Input 
+                  iconName="mail"
+                  editable= {false}
+                  defaultValue={user.email}
+                />
+                <Input 
+                  iconName="credit-card"
+                  placeholder='CNH'
+                  keyboardType='numeric'      
+                  defaultValue={user.driver_license}          
+                />
+              </Section>
+              :
+              <Section>
+                <PasswordInput 
+                  iconName="lock"
+                  placeholder='Senha atual'
+                />
+                <PasswordInput 
+                  iconName="lock"
+                  placeholder='Nova senha'
+                />
+                <PasswordInput 
+                  iconName="lock"
+                  placeholder='Repetir senha'         
+                />
+              </Section>
+
+            }
+
+          </Content>
+        </Container>
+      </TouchableWithoutFeedback>
+    </KeyboardAvoidingView>
   );
 }
--- src/screens/Profile/styles.ts
+++ src/screens/Profile/styles.ts
@@ -8,7 +8,6 @@ interface OptionProps {
 }
 
 export const Container = styled.View`
-  flex: 1;
   background-color: ${({ theme }) => theme.colors.background_primary};
 `;
 
@@ -70,7 +69,6 @@ export const PhotoButton = styled(RectButton)`
 
 
 export const Content = styled.View`
-  flex: 1;
   padding: 0 24px;
   margin-top: 122px;
 `;
